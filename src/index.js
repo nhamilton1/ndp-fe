20e853875b183c87e84de74296b4993f972bafea
nhamilton1 <nickh3592@gmail.com>
2022-01-02 18:54:52 -0500

    removes commented code

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 import { QueryClientProvider, QueryClient } from 'react-query'
-// import { ReactQueryDevtools } from 'react-query/devtools'
 
 const queryClient = new QueryClient()
 
@@ -11,7 +10,6 @@ ReactDOM.render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <App />
-      {/* <ReactQueryDevtools initialIsOpen={false}/> */}
     </QueryClientProvider>
   </React.StrictMode>,
   document.getElementById('root')
